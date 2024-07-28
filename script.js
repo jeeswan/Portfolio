ea5a5d27c5fbdaedfa8bd3ae3e3c5482562d5081
Jeeswan Bajracharya <bajrajeeswan@gmail.com>
2024-07-28 21:55:25 +0545

    update

--- script.js
+++ script.js
@@ -15,10 +15,17 @@ function openTab(tabname){
 // save contact info in a google sheet
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyN9TpNWiazHBNj4ifjkq-osvej5RajD51LOQJjjopX1nNjZ5oiRiUYm1CEgeUj0brr/exec'
   const form = document.forms['submit-to-google-sheet']
+  constmsg = document.getElementById("msg")
 
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
+      .then(response => {
+        msg.innerHTML = "Message sent successfully!!!"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
       .catch(error => console.error('Error!', error.message))
   })
