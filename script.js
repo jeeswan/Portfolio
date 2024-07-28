eaff1ffdf6c064b3f7c62f455d21cc6ea5f67286
Jeeswan Bajracharya <bajrajeeswan@gmail.com>
2024-07-28 19:42:31 +0545

    commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// form validation
+
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     
@@ -11,3 +13,38 @@ document.querySelector('form').addEventListener('submit', function(event) {
         alert('Please fill out all fields.');
     }
 });
+
+// about section
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function openTab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    } 
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// for small screen
+
+var sidemenu = document.getElementById(sidemenu);
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+
+// save contact info in a google sheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyN9TpNWiazHBNj4ifjkq-osvej5RajD51LOQJjjopX1nNjZ5oiRiUYm1CEgeUj0brr/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
